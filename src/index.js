5ee2e1cb3d4a9c8f7d628120ab41b2b375d6f98f
Eric Henderson <henderea@gmail.com>
2019-06-20 14:39:51 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+require('./index.scss');
+const $ = require('jquery');
+// require('jquery-ui');
+// const _ = require('lodash');
+// const rand = require('lodash/random');
+
+import registerServiceWorker from '@henderea/static-site-builder/registerServiceWorker';
+registerServiceWorker();
